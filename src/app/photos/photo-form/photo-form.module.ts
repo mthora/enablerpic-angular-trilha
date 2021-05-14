d401f7bdb216d02b97bf1c8fd89053381c18ba4e
Matheus Teixeira <matheus.teixeira@itenabler.com.br>
2021-05-14 14:41:58 -0300

    Botão de upload auto-clicado ao navegar para tela de upload

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -5,6 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { ImmediateClickModule } from 'src/app/shared/directives/immediate-click/immediate-click.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
@@ -14,7 +15,8 @@ import { CommonModule } from '@angular/common';
       VMessageModule,
       FormsModule,
       RouterModule,
-      PhotoModule
+      PhotoModule,
+      ImmediateClickModule
      ]
 })
 export class PhotoFormModule { }
