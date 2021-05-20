fbb5fa587355b50831f5922c3d9004d306bf135d
Matheus Teixeira <matheus.teixeira@itenabler.com.br>
2021-05-20 09:08:19 -0300

    Upload e progresso

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { AuthGuard } from './core/auth/auth.guard';
+import { PhotoDetailsComponent } from './photos/photo-details/photo-details.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        loadChildren: './home/home.module#HomeModule'
+    },
+    {
+        path: 'user/:userName',
+        pathMatch: 'full',
+        component: PhotoListComponent,
+        resolve: {
+            photos: PhotoListResolver
+        },
+        data: {
+          title: 'Profile'
+        }
+    },
+    {
+        path: 'p/add',
+        component: PhotoFormComponent,
+        canActivate: [AuthGuard],
+        data: {
+          title: 'Upload'
+        }
+    },
+    {
+        path: 'p/:photoId',
+        component: PhotoDetailsComponent,
+        data:{
+          title: 'Photo'
+        }
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+        data: {
+          title: 'Not Found - 404'
+        }
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes, { useHash: true } )
+    ],
+    exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
+
